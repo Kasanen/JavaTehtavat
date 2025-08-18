fe64e437147e936df4595dcb6d7547e9e78594f4
Kasanen <veijok119@gamil.com>
2025-08-18 11:55:12 +0300

    Tehtavat1-4

--- 1.1_Java_as_a_programming_language/src/Task1.java
+++ 1.1_Java_as_a_programming_language/src/Task1.java
@@ -1,5 +1,6 @@
-public class App {
+public class Task1 {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("My name is Veijo.");
     }
 }
---
+++ 1.1_Java_as_a_programming_language/src/Task2.java
@@ -0,0 +1,5 @@
+public class Task2 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("   * \n  *** \n ***** \n*******");
+    }
+}
